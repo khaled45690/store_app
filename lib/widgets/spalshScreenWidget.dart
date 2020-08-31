342834d65665b295ee71bc6a9911d52c9e09054e
Omar-ramadan1 <53654928+Omar-ramadan1@users.noreply.github.com>
2020-08-31 08:34:04 -0700

    kkk

--- lib/widgets/spalshScreenWidget.dart
+++ lib/widgets/spalshScreenWidget.dart
@@ -9,7 +9,7 @@ class SpashScreenWidget extends StatelessWidget {
     return SplashScreen(
       seconds:5,
       backgroundColor: Colors.black,
-      title: Text("صوره كلهااا عظمه",style: TextStyle(fontSize:44,fontWeight:FontWeight.bold,color: Colors.blue),),
+      title: Text("صوره كلهااا عظمه والي يقول غير كده خول ههه",style: TextStyle(fontSize:24,fontWeight:FontWeight.bold,color: Colors.blue),),
       image: Image.network('https://scontent.faly2-1.fna.fbcdn.net/v/t1.0-9/95945567_3178758682182826_597523516736143360_o.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=aMoNQW5sb5gAX8dFvZr&_nc_ht=scontent.faly2-1.fna&oh=97143d52f2d7cfc8119e05870d71fda5&oe=5F711785'),
       loaderColor: Colors.white,
       photoSize: 150,
